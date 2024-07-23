0796d38f3d3c4b09ceac42b967d6a6d988320ee7
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-07-23 13:14:56 +0530

    OPTIMISE THE CODE

--- DAY5/server.js
+++ DAY5/server.js
@@ -2,23 +2,29 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path');
 const server = http.createServer((req, res) => {
+    let filepath = ''
     if (req.url === '/') {
-        const filePath = path.join(__dirname, 'public', 'home.html')
-        fs.readFile(filePath, 'utf-8', function (error, data) {
-            if (error) {
-                console.log('Unable to send file');
-            }
-            else {
-                res.end(data);
-            }
+        filepath = path.join(__dirname, 'public', 'home.html')
 
-        })
+    }
+    else if (req.url == '/login') {
+        filepath = path.join(__dirname, 'public', 'login.html')
+
+    }
+    else if (req.url === '/signup') {
+        filepath = path.join(__dirname, 'public', 'signup.html')
+
     }
     else {
-        res.end('Route not found')
+        res.statusCode = 404
+        res.end('Not Found')
+        return;
     }
 
-
+    fs.readFile(filepath, 'utf-8', function (error, data) {
+        // res.writeHead(200, 'Content-Type', 'text/html')
+        res.end(data);
+    })
 });
 
 server.listen(3600);
