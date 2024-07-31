f2234a690effde2001d12a747e589309ae9e74d4
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-07-31 16:24:00 +0530

    CRUD OPERATION IN JSON

--- DAY8/index.js
+++ DAY8/index.js
@@ -3,14 +3,15 @@ const { studentRouter } = require('./routes/routes.student')
 const { teacherRouter } = require('./routes/routes.teacher')
 const app = express();
 
-
 //middlware
+
+app.use(express.json())
 app.use('/student', studentRouter)
 app.use('/teacher', teacherRouter)
 
-
-
-
+app.get('/', (req, res) => {
+    res.send('Dashboard')
+})
 
 app.listen(3500, () => {
     console.log('server is running')
--- DAY8/routes/routes.student.js
+++ DAY8/routes/routes.student.js
@@ -1,25 +1,59 @@
 const express = require('express')
+const fs = require('fs')
 const studentList = require('../data/student.json');
+const { error } = require('console');
 const studentRouter = express.Router();
 
 studentRouter.get('/', (req, res) => {
-
     res.json(studentList)
-
-
 })
 studentRouter.get('/:id', (req, res) => {
     const id = req.params.id
     const student = studentList.find((std) => std.id == id)
     res.json(student)
-
-
 })
 studentRouter.post('/', (req, res) => {
+    const lastId = studentList[studentList.length - 1].id + 1;
+    // const newuser = {
+    //     id: lastId,
+    //     ...req.body
+    // }
+    const newUser = Object.assign({ id: lastId }, req.body);
+    studentList.push(newUser);
+    console.log(studentList)
+    fs.writeFile('./data/student.json', JSON.stringify(studentList), error => {
+        if (error) {
+            console.log(error)
+        }
+    })
+
+
     res.send('Student created')
 })
-studentRouter.patch('/', (req, res) => {
+
+studentRouter.patch('/:id', (req, res) => {
+    const _id = req.params.id;
+    const StudentToUpdate = studentList.find(std => std.id == _id)
+    const Index = studentList.indexOf(StudentToUpdate)
+    studentList[Index] = Object.assign(StudentToUpdate, req.body)
+    console.log(studentList)
     res.send('Student updated')
+})
+studentRouter.delete('/:id', (req, res) => {
+    const _id = req.params.id;
+    const StudentToUpdate = studentList.find(std => std.id == _id)
+    const Index = studentList.indexOf(StudentToUpdate)
+    studentList.splice(Index, 1);
+    fs.writeFile('./data/student.json', JSON.stringify(studentList), error => {
+        if (error) {
+            console.log(error)
+        }
+        else {
+            res.send('User Deleted')
+        }
+    })
+
+
 })
 module.exports = {
     studentRouter
