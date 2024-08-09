629274c580da431be8899494664539876dc04cc4
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-08-09 19:17:39 +0530

    Assignment upload day 14 MCV

--- DAY13/routes/student.routes.js
+++ DAY13/routes/student.routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const { getAllStudents, createStudent, updateStudent, deleteStudent, getSingleStudent } = require('../controllers/student.controller') 
 const studentRouter = express.Router();
-
 studentRouter.get('/', getAllStudents)
 studentRouter.post('/', createStudent)
 studentRouter.patch('/:id', updateStudent)
