8b9af9704fa3ff059cc95393cc5ba8b0ac378ecc
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-08-11 17:58:18 +0530

    mongo altas

--- DAY13/db.js
+++ DAY13/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const main = async (req, res) => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/userlist')
+        await mongoose.connect('mongodb+srv://mycluster:admin@atlascluster.xlm0rrn.mongodb.net/teacherDb?retryWrites=true&w=majority&appName=AtlasCluster')
         console.log('Database Connected')
     } catch (error) {
         console.log(err)
